e88fc48490a601b7e2d55ff4d0b883c9a9acb029
GoogelTrad <celianmichez@free.fr>
2023-09-05 13:07:41 +0200

    init git

---
+++ philo/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmichez <cmichez@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 11:20:43 by cmichez           #+#    #+#             */
+/*   Updated: 2023/09/05 11:20:43 by cmichez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+
+typedef struct s_philo
+{
+    pthread_t   tid;
+} t_philo;
+
+#endif
